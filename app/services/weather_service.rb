516cecf7c718beac7ba3c2e1de00d80f7545e99c
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-26 10:38:28 -0500

    Add method to service, test passing.

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -10,6 +10,17 @@ class WeatherService
     parse(response)
   end
 
+  def self.current_weather(latitude, longitude)
+    response = connection.get do |f|
+      f.params['lat'] = latitude
+      f.params['lon'] = longitude
+      f.params['appid'] = ENV['WEATHER_KEY']
+      f.params['units'] = 'imperial'
+      f.params['exclude'] = 'minutely,hourly,daily'
+    end
+    parse(response)
+  end
+
   private
     def self.connection
       conn ||= Faraday.new(url: 'https://api.openweathermap.org/data/2.5/onecall')
