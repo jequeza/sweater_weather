df72795ec15ecdb72db2b97ad1b31348d01c28f5
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-26 12:57:47 -0500

    fix test got one error

--- spec/poros/salaries_current_weather_spec.rb
+++ spec/poros/salaries_current_weather_spec.rb
@@ -3,21 +3,26 @@ require 'rails_helper'
 RSpec.describe SalariesCurrentWeather do
   it "exists" do
 
-    data = {:dt=>1619455093,
-     :sunrise=>1619437916,
-     :sunset=>1619485513,
-     :temp=>74.4344444,
-     :feels_like=>74.57,
-     :pressure=>1014,
-     :humidity=>64,
-     :dew_point=>61.47,
-     :uvi=>7.95,
-     :clouds=>100,
-     :visibility=>10000,
-     :wind_speed=>11.5,
-     :wind_deg=>150,
-     :wind_gust=>21.85,
-     :weather=>[{:id=>804, :main=>"Clouds", :description=>"Hello World", :icon=>"04d"}]}
+    data = {:lat=>30.265,
+ :lon=>-97.7466,
+ :timezone=>"America/Chicago",
+ :timezone_offset=>-18000,
+ :current=>
+  {:dt=>1619459712,
+   :sunrise=>1619437916,
+   :sunset=>1619485513,
+   :temp=>73.232,
+   :feels_like=>78.06,
+   :pressure=>1014,
+   :humidity=>57,
+   :dew_point=>61.45,
+   :uvi=>9.61,
+   :clouds=>100,
+   :visibility=>10000,
+   :wind_speed=>10.36,
+   :wind_deg=>150,
+   :wind_gust=>25.32,
+   :weather=>[{:id=>804, :main=>"Clouds", :description=>"Hello World", :icon=>"04d"}]}}
 
 
     forecast = SalariesCurrentWeather.new(data)
@@ -25,7 +30,7 @@ RSpec.describe SalariesCurrentWeather do
     expect(forecast).to be_a SalariesCurrentWeather
     expect(forecast.summary).to eq('Hello World')
     expect(forecast.summary).to be_a String
-    expect(forecast.temperature).to eq("74 F")
+    expect(forecast.temperature).to eq("73 F")
     expect(forecast.temperature).to be_a String
   end
 end
