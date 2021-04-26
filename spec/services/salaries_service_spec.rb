97a40f32dd1fe4729fb670842f353ab930cacd09
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-26 11:15:17 -0500

    Finilize test for SalariesService.

--- spec/services/salaries_service_spec.rb
+++ spec/services/salaries_service_spec.rb
@@ -7,7 +7,13 @@ RSpec.describe SalariesService do
         location = 'austin'
 
         salaries_info = SalariesService.get_salaries(location)
-        require "pry"; binding.pry
+        expect(salaries_info).to be_a Hash
+        expect(salaries_info).to have_key(:salaries)
+        expect(salaries_info[:salaries]).to be_an Array
+        expect(salaries_info[:salaries].first).to have_key(:job)
+        expect(salaries_info[:salaries].first).to have_key(:salary_percentiles)
+        expect(salaries_info[:salaries].first[:job][:title]).to be_a String
+        expect(salaries_info[:salaries].first[:salary_percentiles][:percentile_25]).to be_a Float
       end
     end
   end
