c4ce0bbcaa62c2d90208ad1258436a742bac1682
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-26 21:54:15 -0500

    Fix typos in test.

--- spec/requests/api/v1/background_image_request_spec.rb
+++ spec/requests/api/v1/background_image_request_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Background Image Api' do
   describe 'happy path' do
-    it "sends background image for a given city" do
+    it "sends background image for a given city", :vcr do
       location = 'denver,co'
       get "/api/v1/backgrounds?location=#{location}"
       expect(response).to be_successful
 
       background = JSON.parse(response.body, symbolize_names: true)
+
       expect(background).to have_key(:data)
       expect(background[:data]).to have_key(:type)
       expect(background[:data]).to have_key(:id)
@@ -18,15 +19,15 @@ RSpec.describe 'Background Image Api' do
       expect(background[:data][:attributes][:image]).to have_key(:location)
       expect(background[:data][:attributes][:image][:location]).to eq(location)
       expect(background[:data][:attributes][:image][:location]).to be_a String
-      expect(background[:data][:attributes][:image]).to have_key(:imgae_url)
-      expect(background[:data][:attributes][:image][:imgae_url]).to be_a String
-      expect(background[:data][:attributes][:image]).to have_key(:credit)
-      expect(background[:data][:attributes][:image][:credit]).to have_key(:source)
-      expect(background[:data][:attributes][:image][:credit][:source]).to be_a String
-      expect(background[:data][:attributes][:image][:credit]).to have_key(:author)
-      expect(background[:data][:attributes][:image][:credit][:author]).to be_a String
-      expect(background[:data][:attributes][:image][:credit]).to have_key(:logo)
-      expect(background[:data][:attributes][:image][:credit][:author]).to be_a String
+      expect(background[:data][:attributes][:image]).to have_key(:image_url)
+      expect(background[:data][:attributes][:image][:image_url]).to be_a String
+      expect(background[:data][:attributes]).to have_key(:credit)
+      expect(background[:data][:attributes][:credit]).to have_key(:source)
+      expect(background[:data][:attributes][:credit][:source]).to be_a String
+      expect(background[:data][:attributes][:credit]).to have_key(:author)
+      expect(background[:data][:attributes][:credit][:author]).to be_a String
+      expect(background[:data][:attributes][:credit]).to have_key(:logo)
+      expect(background[:data][:attributes][:credit][:author]).to be_a String
     end
   end
 end
