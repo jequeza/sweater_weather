f102c8054562491684d6a52b29cf19f61aa199d4
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-27 23:01:19 -0500

    Add sad path test for RoadTripFacade for impossible trip.

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -16,6 +16,17 @@ RSpec.describe RoadTripFacade do
         expect(trip.weather_at_eta.conditions).to be_a String
         expect(trip.weather_at_eta.temperature).to be_a Float
       end
+      it "sad path returns impossible for a trip that can not be possible", :vcr do
+        from = 'chicago,il'
+        to = 'london,uk'
+
+        trip = RoadTripFacade.road_trip(from, to)
+        expect(trip.travel_time).to eq("impossible trip")
+        expect(trip.start_city).to eq(from)
+        expect(trip.end_city).to eq(to)
+        expect(trip.weather_at_eta.conditions).to be_nil
+        expect(trip.weather_at_eta.temperature).to be_nil
+      end
     end
   end
 end
